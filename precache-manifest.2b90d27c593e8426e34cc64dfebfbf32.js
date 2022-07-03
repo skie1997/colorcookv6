c277b977429e05845d319e94f18ec8f94c91d24b
skie1997 <skie1997@outlook.com>
2022-07-04 03:00:33 +0800

    Updates

---
+++ precache-manifest.2b90d27c593e8426e34cc64dfebfbf32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "723b9a87874e73c564f00fa81ed5b695",
+    "url": "/colorcookv6/index.html"
+  },
+  {
+    "revision": "6342340517ddaf254c10",
+    "url": "/colorcookv6/static/css/2.7b6a2a93.chunk.css"
+  },
+  {
+    "revision": "612e2ed7829fdc13d11c",
+    "url": "/colorcookv6/static/css/main.9c2a838a.chunk.css"
+  },
+  {
+    "revision": "6342340517ddaf254c10",
+    "url": "/colorcookv6/static/js/2.9d7195b3.chunk.js"
+  },
+  {
+    "revision": "612e2ed7829fdc13d11c",
+    "url": "/colorcookv6/static/js/main.14917dd1.chunk.js"
+  },
+  {
+    "revision": "fa9fa2baf7c566ee25d4",
+    "url": "/colorcookv6/static/js/runtime~main.5d5fcc60.js"
+  }
+]);
