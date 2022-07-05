67633837ff7cef9c3c2a41376a1e8a9fc37007ca
skie1997 <skie1997@outlook.com>
2022-07-05 17:52:36 +0800

    update router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import EditorView from './views/EditorView';
 
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 const customHistory = require("history").createBrowserHistory;
 
 class App extends Component {
   render() {
     return (
-        <Router history  = {customHistory}>
+        <Router history  = {customHistory} basename="./system">
             <Route exact path="/" component={EditorView} />
             <Route exact path="/editor" component={EditorView} />
         </Router>
